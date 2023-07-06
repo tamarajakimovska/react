542e11348bcde908ea7bd34467cbe0c040b9a300
NikolaStoilkovski17 <tamarajakimovska007@gmail.com>
2023-07-06 17:01:40 +0200

    create-react-app

---
+++ react-app/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+import AddContact from "./AddContacts";
+import ContactList from "./ContactList";
+
+function App() {
+  return (
+    <Header />
+   // <AddContact />
+   // <ContactList />
+  );
+}
+
+export default App;
